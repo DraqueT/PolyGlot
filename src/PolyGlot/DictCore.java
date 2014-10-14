4162bf3a6329c403151b2449c1fa159af72c45de
Draque Thompson <draque@192.168.168.71>
2014-10-14 19:26:58 -0400

    updates version number

--- src/PolyGlot/DictCore.java
+++ src/PolyGlot/DictCore.java
@@ -51,7 +51,7 @@ import org.xml.sax.SAXException;
 
 public class DictCore {
 
-    private final String version = "0.8";
+    private final String version = "0.8.1";
     private final ConWordCollection wordCollection = new ConWordCollection(this);
     private final TypeCollection typeCollection = new TypeCollection();
     private final GenderCollection genderCollection = new GenderCollection();
