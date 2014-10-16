06e576bbd9d6a230e49d5264cbcc54d6b3764565
Draque Thompson <draque@192.168.10.105>
2014-10-15 22:33:47 -0400

    Patch for error #65. Eliminates font archival in windows.

--- src/PolyGlot/IOHandler.java
+++ src/PolyGlot/IOHandler.java
@@ -141,7 +141,7 @@ public class IOHandler {
             }
         } else if (System.getProperty("os.name").startsWith("Windows")) {
             // TODO: THIS NEEDS TO BE TESTED IN WINDOWS
-            ret = getFontFromLocation(System.getenv("WINDIR") + "\\Fonts", font);
+            //ret = getFontFromLocation(System.getenv("WINDIR") + "\\Fonts", font);
         } else {
             // Other OSes don't support this yet
         }
