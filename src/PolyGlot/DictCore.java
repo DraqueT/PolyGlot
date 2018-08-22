d2019a0ddd5ea99e2b05fca0e0e81b98e2f000b1
DThompson <DThompson@10.13.8.136>
2018-08-22 11:38:05 -0400

    updates version number

--- src/PolyGlot/DictCore.java
+++ src/PolyGlot/DictCore.java
@@ -57,7 +57,7 @@ import org.xml.sax.SAXException;
 
 public class DictCore {
 
-    private final String version = "2.3.2";
+    private final String version = "2.3.3";
     private ConWordCollection wordCollection;
     private TypeCollection typeCollection;
     private DeclensionManager declensionMgr;
@@ -636,6 +636,7 @@ public class DictCore {
         versionHierarchy.put("2.2", 30);
         versionHierarchy.put("2.3", 31);
         versionHierarchy.put("2.3.1", 32);
-        versionHierarchy.put("2.3.2", 32);
+        versionHierarchy.put("2.3.2", 33);
+        versionHierarchy.put("2.3.3", 34);
     }
 }
