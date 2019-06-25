fa54825e900b5ec892b57f4a4c20cf56628fec2d
DraqueT <draquemail@gmail.com>
2019-06-25 16:56:42 -0700

    further updates to #709

--- post_build.sh
+++ post_build.sh
@@ -39,21 +39,27 @@ rm -r "${tmp_folder}"
 
 file_dest="/Users/draque/Google Drive/Permanent_Share/PolyGlot_BETA.zip"
 file_orig="PolyGlot_BETA.zip"
-beta_warn_dest="dist/BETA_WARNING.txt"
+beta_warn_dest_1="dist/BETA_WARNING.txt"
+beta_warn_dest_2="dist/lib/BETA_WARNING.txt"
 beta_warn_orig="BETA_WARNING.txt"
+frontend_dist="dist/PolyGlot Frontend.exe"
 
 if [ -f "$file_dest" ]
 then
         echo "Creating Beta Build to upload..."
-	cp "${beta_warn_orig}" "${beta_warn_dest}" 
+	cp "${beta_warn_orig}" "${beta_warn_dest_1}" 
+        cp "${beta_warn_orig}" "${beta_warn_dest_2}" 
         cp "${frontend}" dist
-	echo -e "\n\nCompiled:" >> "${beta_warn_dest}"
-	date >> "${beta_warn_dest}"
-	cat >> "${beta_warn_dest}"
+	echo -e "\n\nCompiled:" >> "${beta_warn_dest_1}"
+	date >> "${beta_warn_dest_1}"
+	cat >> "${beta_warn_dest_1}"
 	echo Archiving PolyGlot Beta...
-	zip -r "${file_orig}" dist
-	echo Copying to public share folder...
+	zip -r "${file_orig}" dist/lib
+        zip -ru "${file_orig}" dist/BETA_WARNING.txt
+        zip -ru "${file_orig}" "${frontend_dist}"
+        zip -ru "${file_orig}" dist/PolyGlot.jar
+	echo "Copying to public share folder..."
 	mv "${file_orig}" "${file_dest}"
 else
-	echo "Expected folder structure mission. Beta not uploaded."
+ 	echo "Expected folder structure mission. Beta not uploaded."
 fi
