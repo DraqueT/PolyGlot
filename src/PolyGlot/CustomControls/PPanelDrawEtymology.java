29cad5ca6ff962f505710fdb787cf820b1a24c92
DraqueT <draquemail@gmail.com>
2019-06-25 19:44:18 -0700

    update copyrights

--- src/PolyGlot/CustomControls/PPanelDrawEtymology.java
+++ src/PolyGlot/CustomControls/PPanelDrawEtymology.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018, Draque Thompson
+ * Copyright (c) 2017-2019, Draque Thompson
  * All rights reserved.
  *
  * Licensed under: Creative Commons Attribution-NonCommercial 4.0 International Public License
