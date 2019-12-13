95d47e291ab59b6092cf9af90679cc6be623bccb
Draque <polyglot@PolyGlotUbuntu>
2019-12-12 23:23:38 -0800

    updates stupid test assumptions

--- src/test/java/org/darisadesigns/polyglotlina/PFontHandlerTest.java
+++ src/test/java/org/darisadesigns/polyglotlina/PFontHandlerTest.java
@@ -20,6 +20,11 @@
 package org.darisadesigns.polyglotlina;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,23 +38,38 @@ public class PFontHandlerTest {
     
     public PFontHandlerTest() {
     }
+    
+    /**
+     * Runs to ensure that the correct files are the most recently updated
+     */
+    @BeforeAll
+    public static void setupTests() {
+        String path = PGTUtil.TESTRESOURCES + File.separator + "FontsAll" + File.separator + "Kukun_LinearA_V05.ttf";
+        File file1 = new File(path);
+        File file2 = new File(path + "X");
+        try {
+            Files.copy(file1.toPath(), file2.toPath());
+            file1.delete();
+            Files.copy(file2.toPath(), file1.toPath());
+            file2.delete();
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
 
     @Test
     public void testGetFontFromLocationsAllSingleFolder() {
-        //Kukun_Linear_A
-        long expectedLastEditDate = 1551291313361L;
+        System.out.println("PFontHandlerTest.testGetFontFromLocationsAllSingleFolder");
         String expectedFileName = "Kukun_LinearA_V05.ttf";
         
         File file = PFontHandler.getFontFromLocations(FONT_FAM, PGTUtil.TESTRESOURCES + File.separator + "FontsAll");
         
         assertEquals(file.getName(), expectedFileName);
-        assertEquals(file.lastModified(), expectedLastEditDate);
     }
     
     @Test
     public void testGetFontFromLocationsAllTwoFolders() {
-        //Kukun_Linear_A
-        long expectedLastEditDate = 1551291313361L;
+        System.out.println("PFontHandlerTest.testGetFontFromLocationsAllTwoFolders");
         String expectedFileName = "Kukun_LinearA_V05.ttf";
         
         File file = PFontHandler.getFontFromLocations(FONT_FAM, 
@@ -58,11 +78,11 @@ public class PFontHandlerTest {
         );
         
         assertEquals(file.getName(), expectedFileName);
-        assertEquals(file.lastModified(), expectedLastEditDate);
     }
     
     @Test
     public void testGetFontFromLocationsEmptyLocation() {
+        System.out.println("PFontHandlerTest.testGetFontFromLocationsEmptyLocation");
         File file = PFontHandler.getFontFromLocations(FONT_FAM, 
                 PGTUtil.TESTRESOURCES + File.separator + "EmptyFolder");
         
@@ -71,6 +91,7 @@ public class PFontHandlerTest {
     
     @Test
     public void testGetFontFromLocationsBadLocation() {
+        System.out.println("PFontHandlerTest.testGetFontFromLocationsBadLocation");
         File file = PFontHandler.getFontFromLocations(FONT_FAM, 
                 PGTUtil.TESTRESOURCES + File.separator + "BIBBITYBOBBITYBOO");
         
