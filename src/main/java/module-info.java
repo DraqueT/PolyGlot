542586dd5c5f667e2aa7f5a6e3d9777ac2750e99
Draque Thompson <draque@accretivetg.com>
2020-02-27 17:29:50 -0800

    Cleanup related to prep for Java 14... good to have OpenJDK14 placed into all environments once it drops.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module org.darisadesigns.polyglotlina.polyglot {
     requires java.desktop;
     requires java.logging;
     requires org.apache.commons.lang3; //AUT - fixed with module injector
-    requires org.apache.commons.text; //AUT - fixed with module injector
     requires org.jsoup; // AUT - fixed with module injector
 
     exports org.darisadesigns.polyglotlina;
