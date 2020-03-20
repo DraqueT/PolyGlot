089c75dc32b275c3fc1aee36ac2da0701effd872
Draque Thompson <draque@accretivetg.com>
2020-03-20 14:13:51 -0700

    Updates version.

--- src/main/java/org/darisadesigns/polyglotlina/PGTUtil.java
+++ src/main/java/org/darisadesigns/polyglotlina/PGTUtil.java
@@ -537,6 +537,7 @@ public final class PGTUtil {
         VERSION_HIERARCHY.put("3.0", 37);
         VERSION_HIERARCHY.put("3.0.1", 38);
         VERSION_HIERARCHY.put("3.1", 39);
+        VERSION_HIERARCHY.put("3.1.1", 40);
         
         // Gather build date/time from resources (if it does not exist, ignore)
         URL buildDate = PGTUtil.class.getResource(BUILD_DATE_TIME_LOCATION);
