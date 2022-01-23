831056b3baa2cc54d2e9415d35844901ff42081a
Draque Thompson <draque@accretivetg.com>
2022-01-23 15:12:10 -0800

    build updates

--- build_image.py
+++ build_image.py
@@ -224,7 +224,7 @@ def dist():
 
 def buildLinux():
     print('cleaning/testing/compiling...')
-    os.system('mvn clean package')
+    os.system('﻿/usr/lib/jvm/apache-maven-3.8.4/bin/mvn clean package')
 
 def cleanLinux():
     print('cleaning build paths...')
