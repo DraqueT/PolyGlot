261d60510f43480c13b2d5c35575a63a923b0c8c
Draque Thompson <draque@accretivetg.com>
2022-01-26 01:27:10 -0800

    Corrects and closes #1128

--- src/main/java/org/darisadesigns/polyglotlina/Desktop/CustomControls/PToolTipUI.java
+++ src/main/java/org/darisadesigns/polyglotlina/Desktop/CustomControls/PToolTipUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, Draque Thompson
+ * Copyright (c) 2017-2022, Draque Thompson
  * All rights reserved.
  *
  * Licensed under: MIT Licence
@@ -28,8 +28,6 @@ import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 import javax.swing.JComponent;
@@ -37,9 +35,8 @@ import javax.swing.JToolTip;
 import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.ToolTipUI;
-import javax.swing.plaf.basic.BasicHTML;
-import javax.swing.text.View;
+import javax.swing.plaf.synth.SynthContext;
+import javax.swing.plaf.synth.SynthToolTipUI;
 import org.darisadesigns.polyglotlina.Desktop.PolyGlot;
 import org.darisadesigns.polyglotlina.PGTUtil;
 
@@ -49,12 +46,9 @@ import org.darisadesigns.polyglotlina.PGTUtil;
  * manner)
  * @author DThompson
  */
-public class PToolTipUI extends ToolTipUI {
+public class PToolTipUI extends SynthToolTipUI {
 
     private static final PToolTipUI SHARED_INSTANCE = new PToolTipUI();
-    private static PropertyChangeListener sharedPropertyChangedListener;
-    private PropertyChangeListener propertyChangeListener;
-    
     private Font font;
     private String lastTextRendered = "";
     private int height = 0;
@@ -63,81 +57,19 @@ public class PToolTipUI extends ToolTipUI {
     private Rectangle textRectangle;
     private FontMetrics metrics;
     
-    public PToolTipUI() {
-        super();
-    }
-    
     public static ComponentUI createUI(JComponent c) {
         return SHARED_INSTANCE;
-    }
-
-    @Override
-    public void installUI(JComponent c) {
-        installDefaults(c);
-        installComponents(c);
-        installListeners(c);
-    }
-
-    @Override
-    public void uninstallUI(JComponent c) {
-        // REMIND: this is NOT getting called
-        uninstallDefaults(c);
-        uninstallComponents(c);
-        uninstallListeners(c);
-    }
-
-    protected void installDefaults(JComponent c){
-        LookAndFeel.installColorsAndFont(c, "ToolTip.background",
-                                         "ToolTip.foreground",
-                                         "ToolTip.font");
-        LookAndFeel.installProperty(c, "opaque", Boolean.TRUE);
-        componentChanged(c);
-    }
-
-   protected void uninstallDefaults(JComponent c){
-        LookAndFeel.uninstallBorder(c);
-    }
-
-    /* Unfortunately this has to remain private until we can make API additions.
-     */
-    private void installComponents(JComponent c){
-        BasicHTML.updateRenderer(c, ((JToolTip)c).getTipText());
-    }
-
-    /* Unfortunately this has to remain private until we can make API additions.
-     */
-    private void uninstallComponents(JComponent c){
-        BasicHTML.updateRenderer(c, "");
-    }
-
-    protected void installListeners(JComponent c) {
-        propertyChangeListener = createPropertyChangeListener();
-        c.addPropertyChangeListener(propertyChangeListener);
-    }
-
-    protected void uninstallListeners(JComponent c) {
-        c.removePropertyChangeListener(propertyChangeListener);
-
-        propertyChangeListener = null;
-    }
-
-    /* Unfortunately this has to remain private until we can make API additions.
-     */
-    private PropertyChangeListener createPropertyChangeListener() {
-        if (sharedPropertyChangedListener == null) {
-            sharedPropertyChangedListener = new PropertyChangeHandler();
-        }
-        return sharedPropertyChangedListener;
     }
 
     /**
      * To override font, override createToolTip() on component creating tooltip.
      * On creation of ToolTip, change to desired font.
      * @param g
-     * @param c 
+     * @param context
      */
     @Override
-    public void paint(Graphics g, JComponent c) {
+    protected void paint(SynthContext context, Graphics g) {
+        JComponent c = context.getComponent();
         ((Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         String tipText = ((JToolTip)c).getTipText();
         tipText = getFormattedTipText(tipText);
@@ -170,16 +102,7 @@ public class PToolTipUI extends ToolTipUI {
         c.setSize(width, height);
         componentChanged(c);
     }
-    
-    @Override
-    public Dimension getPreferredSize(JComponent c) {
-        if (width == 1) {
-            setRenderingDetails(c);
-        }
-        
-        return new Dimension(width, height);
-    }
-    
+
     /**
      * Sets up all measurements needed for rendering (slightly expensive, do not call more than necessary)
      */
@@ -212,7 +135,7 @@ public class PToolTipUI extends ToolTipUI {
 
         c.setFont(font.deriveFont(fontSize));
     }
-    
+
     /**
      * Formats tip text appropriately with linebreaks if it is not formatted initially.
      * @param text
@@ -263,31 +186,20 @@ public class PToolTipUI extends ToolTipUI {
         
         return ret;
     }
-
+    
     @Override
-    public Dimension getMinimumSize(JComponent c) {
-        Dimension d = getPreferredSize(c);
-        View v = (View) c.getClientProperty(BasicHTML.propertyKey);
-        if (v != null) {
-            d.width -= v.getPreferredSpan(View.X_AXIS) - v.getMinimumSpan(View.X_AXIS);
+    public Dimension getPreferredSize(JComponent c) {
+        if (width == 1 || width == 0) {
+            setRenderingDetails(c);
         }
-        return d;
-    }
-
-    @Override
-    public Dimension getMaximumSize(JComponent c) {
-        Dimension d = getPreferredSize(c);
-        View v = (View) c.getClientProperty(BasicHTML.propertyKey);
-        if (v != null) {
-            d.width += v.getMaximumSpan(View.X_AXIS) - v.getPreferredSpan(View.X_AXIS);
-        }
-        return d;
+        
+        return new Dimension(width, height);
     }
 
     /**
      * Invoked when the {@code JComponent} associated with the
      * <code>JToolTip</code> has changed, or at initialization time. This
-     * should update any state dependant upon the {@code JComponent}.
+     * should update any state dependent upon the {@code JComponent}.
      *
      * @param c the JToolTip the JComponent has changed on.
      */
@@ -299,15 +211,14 @@ public class PToolTipUI extends ToolTipUI {
             // properties if they are defined.
             if (UIManager.getBorder("ToolTip.borderInactive") != null) {
                 LookAndFeel.installBorder(c, "ToolTip.borderInactive");
-            }
-            else {
+            } else {
                 LookAndFeel.installBorder(c, "ToolTip.border");
             }
+            
             if (UIManager.getColor("ToolTip.backgroundInactive") != null) {
                 LookAndFeel.installColors(c,"ToolTip.backgroundInactive",
                                           "ToolTip.foregroundInactive");
-            }
-            else {
+            } else {
                 LookAndFeel.installColors(c,"ToolTip.background",
                                           "ToolTip.foreground");
             }
@@ -315,31 +226,6 @@ public class PToolTipUI extends ToolTipUI {
             LookAndFeel.installBorder(c, "ToolTip.border");
             LookAndFeel.installColors(c, "ToolTip.background",
                                       "ToolTip.foreground");
-        }
-    }
-
-
-    private static class PropertyChangeHandler implements
-                                 PropertyChangeListener {
-        @Override
-        public void propertyChange(PropertyChangeEvent e) {
-            String name = e.getPropertyName();
-            if (name.equals("tiptext") || "font".equals(name) ||
-                "foreground".equals(name)) {
-                // remove the old html view client property if one
-                // existed, and install a new one if the text installed
-                // into the JLabel is html source.
-                JToolTip tip = ((JToolTip) e.getSource());
-                String text = tip.getTipText();
-                BasicHTML.updateRenderer(tip, text);
-            }
-            else if ("component".equals(name)) {
-                JToolTip tip = ((JToolTip) e.getSource());
-
-                if (tip.getUI() instanceof PToolTipUI) {
-                    ((PToolTipUI)tip.getUI()).componentChanged(tip);
-                }
-            }
         }
     }
 }
--- src/main/java/org/darisadesigns/polyglotlina/Desktop/PolyGlot.java
+++ src/main/java/org/darisadesigns/polyglotlina/Desktop/PolyGlot.java
@@ -92,13 +92,11 @@ public final class PolyGlot {
         try {
             // must be set before accessing System to test OS (values will simply be ignored for other OSes
             if (PGTUtil.IS_OSX) {
-                // set program icon
                 Taskbar.getTaskbar().setIconImage(PGTUtil.POLYGLOT_ICON.getImage());
+                System.setProperty("apple.laf.useScreenMenuBar", "true");
+                System.setProperty("apple.awt.application.name", PGTUtil.DISPLAY_NAME);
+                System.setProperty("com.apple.mrj.application.apple.menu.about.name", PGTUtil.DISPLAY_NAME);
             }
-
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-            System.setProperty("apple.awt.application.name", PGTUtil.DISPLAY_NAME);
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", PGTUtil.DISPLAY_NAME);
 
             setupNimbus();
             setupCustomUI();
