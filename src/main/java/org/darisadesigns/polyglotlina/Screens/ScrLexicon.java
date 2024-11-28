996f216c8d225c1fb2fd33333e231e05329236b3
ivaniesta14 <ivaniesta14@gmail.com>
2024-11-28 17:02:23 +0100

    FIxup: Remove unneeded import

--- src/main/java/org/darisadesigns/polyglotlina/Screens/ScrLexicon.java
+++ src/main/java/org/darisadesigns/polyglotlina/Screens/ScrLexicon.java
@@ -42,7 +42,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
